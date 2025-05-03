6692cf6da3ee66973be6684c5409be86b9a21e20
ちりめん <96675462+tirimen-sub@users.noreply.github.com>
2025-05-03 11:10:58 +0900

    change cd

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const AWS     = require('aws-sdk');
 const fs      = require('fs');
 const path    = require('path');
 
-const { pool, init } = require('./initDB');
+const { pool, init } = require('./db/initDB');
 
 const app = express();
 
