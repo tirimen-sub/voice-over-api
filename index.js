5be80704128b38c7bcd21d9854df8812da5c6fef
ちりめん <96675462+tirimen-sub@users.noreply.github.com>
2025-05-03 11:08:26 +0900

    initDB

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const AWS     = require('aws-sdk');
 const fs      = require('fs');
 const path    = require('path');
 
-const { pool, init } = require('./db');
+const { pool, init } = require('./initDB');
 
 const app = express();
 
