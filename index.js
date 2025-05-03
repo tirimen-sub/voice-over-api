785a2e8a5ca0120e85648e7126d72b5f1adec000
ちりめん <96675462+tirimen-sub@users.noreply.github.com>
2025-05-03 17:14:32 +0900

    ss

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const path    = require('path');
 const { pool, init } = require('./db/initDB');
 const app = express();
 
-// CORS設定（必要に応じてオリジンを調整)
+// CORS設定（必要に応じてオリジン調整)
 app.use(cors({
   origin: process.env.CORS_ORIGIN || '*',
   methods: ['GET','POST','PUT','DELETE','OPTIONS'],
